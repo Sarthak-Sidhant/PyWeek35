d63a21610db4797e5372c51e347737c6b713432f
Rellekk-Z <52284479+Sarthak-Sidhant@users.noreply.github.com>
2023-04-01 23:52:37 +0530

    lol test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import time
+print("Pyweek is ending in like 30 something hours.")
+time.sleep(5)
+print("I wanted to commit to this stuff every day. But I can't")
+time.sleep(2)
+print("I want to make a game tomorrow.")
+time.sleep(2)
+print("And I know for a fact that I will make it.")
+time.sleep(1)
+print("goodluck sarthak!")
